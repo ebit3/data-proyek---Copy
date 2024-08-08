f19852b021a15897555447948fe04db699a48a42
Ebit Gunawan <ebitgunawan2@gmail.com>
2024-08-08 20:07:20 +0700

    commit-kamis

---
+++ pengajuan.js
@@ -0,0 +1,56 @@
+document.getElementById('postPengajuan').addEventListener('submit', function (e) {
+	e.preventDefault();
+
+	const name = document.getElementById('name').value;
+	const email = document.getElementById('email').value;
+	const phone = document.getElementById('phone').value;
+	const alamat = document.getElementById('alamat').value;
+	const merek = document.getElementById('merek').value;
+
+	const data = new URLSearchParams();
+	data.append('name', name);
+	data.append('email', email);
+	data.append('phone', phone);
+	data.append('alamat', alamat);
+	data.append('merek', merek);
+
+	const scriptURL =
+		'https://script.google.com/macros/s/AKfycbxYEc2E97qzOMM1oVnSirhUrJT6VGedKz6s1qKechq5902iUYfoET-JeqTMQQUcr2r4/exec';
+
+	// Tampilkan pesan loading
+	document.querySelector('.loading').style.display = 'block';
+	document.querySelector('.error-message').style.display = 'none';
+	document.querySelector('.sent-message').style.display = 'none';
+
+	fetch(scriptURL, {
+		method: 'POST',
+		body: data,
+	})
+		.then((response) => response.text())
+		.then((data) => {
+			console.log('Success:', data);
+
+			// Mengosongkan input
+			document.getElementById('name').value = '';
+			document.getElementById('email').value = '';
+			document.getElementById('phone').value = '';
+			document.getElementById('alamat').value = '';
+			document.getElementById('merek').value = '';
+
+			// Sembunyikan pesan loading dan tampilkan pesan sukses
+			document.querySelector('.loading').style.display =
+				'none';
+			document.querySelector('.sent-message').style.display =
+				'block';
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+
+			// Sembunyikan pesan loading dan tampilkan pesan error
+			document.querySelector('.loading').style.display =
+				'none';
+			document.querySelector(
+				'.error-message'
+			).style.display = 'block';
+		});
+});
